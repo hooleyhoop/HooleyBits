5ff8eeb0ab0953db5d686f51467ef077dd314cf7
HooleyHoop <steven.hooley@gmail.com>
2011-05-15 23:27:59 +0100

    NSConditionlock not working for me

--- MachoLoader/Tests/BasicProcessorSimulatorTests.m
+++ MachoLoader/Tests/BasicProcessorSimulatorTests.m
@@ -11,6 +11,8 @@
 #import "TPData.h"
 #import "TPLine.h"
 #import "ContiguousMemoryBlockStore.h"
+#import <Foundation/NSDebug.h>
+#import <sys/stat.h>
 
 /*
  * I will endevour to TDD a nice layout for the simulator. This wont be the working code
@@ -40,55 +42,74 @@ const char * _TYPE_CODE_ = @encode(__typeof__(_X_));\
 
 
 #pragma mark -
-@interface CallbackProxy : NSObject {
-NSObject *_callbackObject;
-NSThread *_callbackThread;
-SEL _callbackSelector;
-}
-- (void)setup:(NSObject *)o :(NSThread *)t :(SEL)s;
-@end @implementation CallbackProxy
-- (void)dealloc {
-    [_callbackObject release];
-    [super dealloc];
-}
-- (void)setup:(NSObject *)o :(NSThread *)t :(SEL)s {
-    _callbackObject = [o retain];
-    _callbackThread = t;
-    _callbackSelector = s;
-}
-- (void)msg {
-    [_callbackObject performSelector:_callbackSelector onThread:_callbackThread withObject:nil waitUntilDone:NO];
-}
-@end
+//@interface CallbackProxy : NSObject {
+//    NSObject *_callbackObject;
+//    NSThread *_callbackThread;
+//    SEL _callbackSelector;
+//}
+//- (void)setup:(NSObject *)o :(NSThread *)t :(SEL)s;
+//
+//@end @implementation CallbackProxy
+//- (void)dealloc {
+//    [_callbackObject release];
+//    [super dealloc];
+//}
+//- (void)setup:(NSObject *)o :(NSThread *)t :(SEL)s {
+//    _callbackObject = [o retain];
+//    _callbackThread = t;
+//    _callbackSelector = s;
+//}
+//- (void)msg {
+//    [_callbackObject performSelector:_callbackSelector onThread:_callbackThread withObject:nil waitUntilDone:NO];
+//}
+//@end
 
-#pragma mark -
-@interface CallbackProxyTests : SenTestCase {	
-} @end
-@implementation CallbackProxyTests
-int count = 0;
-- (void)_callback {
-    count++;
-}
-- (void)testCallback {
-    CallbackProxy *cp = [[[CallbackProxy alloc] init] autorelease];
-    [cp setup:self :[NSThread currentThread] :@selector(_callback)];
-    [cp msg];
-    STAssertTrue( count==1, nil );
-}
-@end
+//#pragma mark -
+//@interface CallbackProxyTests : SenTestCase {	
+//} @end
+//@implementation CallbackProxyTests
+//int count = 0;
+//- (void)_callback {
+//    count++;
+//}
+//- (void)testCallback {
+//    CallbackProxy *cp = [[[CallbackProxy alloc] init] autorelease];
+//    [cp setup:self :[NSThread currentThread] :@selector(_callback)];
+//    [cp msg];
+//    STAssertTrue( count==1, nil );
+//}
+//@end
 
 #pragma mark -
 @interface AsyncAction : NSObject {
+    
+    /* Note: custom queues are alwys serial and execute one job at a time - this can replace locks - lightweight so this is reasonable */
+    dispatch_queue_t _customQueue;
+    
     NSDictionary *_callbacks;
 }
 
 @end @implementation AsyncAction
+
+- (id)init {
+    self = [super init];
+    _customQueue = dispatch_queue_create("hooley.steve", NULL);    
+    return self;
+}
+
+- (void)dealloc {
+    [super dealloc];
+    [_callbacks release];
+    dispatch_release(_customQueue);
+}
+
 - (void)setCallbacks:(NSDictionary *)callbacks {
     _callbacks = [callbacks retain];
 }
+
 - (void)call:(NSString *)key {
     id ob = [_callbacks objectForKey:key];
-    [ob msg];
+    dispatch_async( _customQueue, ob );
 }
 @end
 
@@ -105,14 +126,10 @@ int count2 = 0;
     count2++;
 }
 - (void)testAsyAction {
-    
-    CallbackProxy *cp1 = [[[CallbackProxy alloc] init] autorelease];
-    [cp1 setup:self :[NSThread currentThread] :@selector(_callback1)];
 
-    CallbackProxy *cp2 = [[[CallbackProxy alloc] init] autorelease];
-    [cp2 setup:self :[NSThread currentThread] :@selector(_callback2)];
-    
-    NSDictionary *callbacks = [NSDictionary dictionaryWithObjectsAndKeys:cp1, @"doit1", cp2, @"doit2", nil];
+    void (^cp1)() = ^ { [self _callback1]; };
+    void (^cp2)() = ^ { [self _callback2]; };
+    NSDictionary *callbacks = [NSDictionary dictionaryWithObjectsAndKeys:[[cp1 copy] autorelease], @"doit1", [[cp2 copy] autorelease], @"doit2", nil];
     
     AsyncAction *action = [[[AsyncAction alloc] init] autorelease];
     [action setCallbacks:callbacks];
@@ -127,33 +144,68 @@ int count2 = 0;
 
 #pragma mark -
 @interface RepeatingAsyncTaskController : NSObject {
-    AsyncAction *_task;
+    AsyncAction     *_task;
+    NSConditionLock *_completionLock;
 }
 - (void)setTask:(AsyncAction *)tsk;
 - (void)go;
-- (void)waitUntillComplete;
+- (void)taskDidComplete;
+- (void)taskDidError;
+- (void)taskIsWaiting;
+- (void)waitUntilAllOperationsAreFinished;
 @end
 @implementation RepeatingAsyncTaskController
+
+- (id)init {
+    self = [super init];
+    _completionLock = [[NSConditionLock alloc] initWithCondition:0];
+    return self;
+}
+
+- (void)dealloc {
+    [_completionLock release];
+    [super dealloc];
+}
 
 - (void)setTask:(AsyncAction *)tsk {
     _task = [tsk retain];
 }
 
 /* these are called on the original thread */
-- (void)taskDidComplete {
-}
-- (void)taskDidError {
-}
 - (void)taskIsWaiting {
-    [_task call:@"step"];
+   [_task call:@"step"];
 }
 
 - (void)go {
+
+    // acquire lock immediately
+    [_completionLock lockWhenCondition:0];
+
+    [_task call:@"step"];
+}
+
+- (void)taskDidComplete {
+    
+    -- grr you cant do this
+        
+    // -- free the lock
+    // unlock in a state that allows thread A to run
+    [_completionLock unlockWithCondition:1];
+}
+
+- (void)taskDidError {
     
 }
-- (void)waitUntillComplete {
-    
+
+- (void)waitUntilAllOperationsAreFinished {
+   // -- wait for the lock
+    // wait for thread B to do something
+    [_completionLock lockWhenCondition:1];
+    // be sure lock is unlocked before releasing it (on thread which created it)
+    [_completionLock unlockWithCondition:0];
+    [_completionLock release];
 }
+
 @end
 
 #pragma mark -
@@ -165,12 +217,12 @@ int count2 = 0;
     //    action = new ASyncAction( instrStepper. step:&address )
     //    instrStepper.callback = action.asyncActionCompleted;        // what thread?
     
-    RepeatingAsyncTaskController *repeatingOp = [[RepeatingAsyncTaskController alloc] init];
-    repeatingOp.action = 
-    repeatingOp.howDoWeKnowWhenFinished?
-    [repeatingOp go];
+//duh    RepeatingAsyncTaskController *repeatingOp = [[RepeatingAsyncTaskController alloc] init];
+//duh    repeatingOp.action = 
+//duh    repeatingOp.howDoWeKnowWhenFinished?
+//duh    [repeatingOp go];
     
-    -- how do we test that it executes three times then action signals that it is complete
+ //duh   -- how do we test that it executes three times then action signals that it is complete
 }
 @end
 
@@ -211,6 +263,7 @@ int count2 = 0;
 @end
 
 #pragma mark -
+
 @interface TPDissemblerTests : SenTestCase {	
 } @end
 @implementation TPDissemblerTests
@@ -224,16 +277,22 @@ int count2 = 0;
 }
 @end
 
+#pragma mark -
 
 @interface StandinForStepper : SenTestCase {
 @public
     int count;
-} @end
+}
+- (void)fakeStep:(AsyncAction *)thisTask;
+@end
 @implementation StandinForStepper
+
 - (void)fakeStep:(AsyncAction *)thisTask {
     [NSThread detachNewThreadSelector: @selector(fakeCatch:) toTarget:self withObject:thisTask];       
 }
+
 - (void)fakeCatch:(AsyncAction *)thisTask {
+    
     [NSThread sleepForTimeInterval:0.5];
     count++;
     if(count==3)
@@ -286,7 +345,7 @@ enum TP_INSTR {
 
     STAssertTrue( [instrStepper step:&address], nil );
     STAssertTrue( address==0, nil );
-    
+
     STAssertTrue( [instrStepper step:&address], nil );
     STAssertTrue( address==3, nil );
 
@@ -295,7 +354,7 @@ enum TP_INSTR {
 
     STAssertTrue( [instrStepper step:&address], nil );
     STAssertTrue( address==8, nil );
-    
+
     STAssertFalse( [instrStepper step:&address], nil );
 }
 
@@ -305,28 +364,28 @@ int timeToDoWork;
     timeToDoWork=0;
     NSCondition *cocoaCondition = [[NSCondition alloc] init];
     [cocoaCondition lock];
-    
+
     NSLog(@"In first thread - launching");
 
     //-- start a new thread that does something after 1 sec
     [NSThread detachNewThreadSelector: @selector(aTestThreadMain:) toTarget:self withObject:cocoaCondition];       
-    
+
     NSLog(@"In first thread - waiting");
 
     while (timeToDoWork <= 0)
         [cocoaCondition wait];
-    
+
     timeToDoWork--;
-    
+
     // Do real work here.
-    
+
     [cocoaCondition unlock];
     [cocoaCondition release];
     NSLog(@"In first thread - compete");   
 }
 
 - (void)aTestThreadMain:(NSCondition *)cocoaCondition {
-    
+
     NSLog(@"In secondry thread");
     [NSThread sleepForTimeInterval:0.5];
 
@@ -342,26 +401,53 @@ int timeToDoWork;
 
 - (void)testThreadCallbackStuff {
     
-    CallbackProxy *callback = [[CallbackProxy alloc] init];
-    [callback setup:self :[NSThread currentThread] :@selector(fakeCallback)];
+//doh    CallbackProxy *callback = [[CallbackProxy alloc] init];
+//doh    [callback setup:self :[NSThread currentThread] :@selector(fakeCallback)];
     
-    [NSThread detachNewThreadSelector: @selector(aTestThreadMain) toTarget:self withObject:callback];
+//doh    [NSThread detachNewThreadSelector: @selector(aTestThreadMain) toTarget:self withObject:callback];
     
     //--oops, crash
-    [callback release];
+//doh    [callback release];
 }
 
 - (void)fakeCallback {
 
 }
-- (void)fakeExceptionThread:(CallbackProxy *)callback {
+- (void)fakeExceptionThread:(id)callback {
     
-    [callback msg];
+//doh    [callback msg];
 }
 
 
 
-
++ (NSString *)releasePoolsAsString {
+    
+	NSString *dataAsString = @"";
+	
+    
+    //		setvbuf( stderr, NULL, _IONBF, 0 );
+    //
+    //		fflush(stderr);
+    int o = dup(fileno(stderr));
+    
+    // pipe way
+    NSPipe *pipe = [NSPipe pipe];
+    NSFileHandle *fileHandleForWriting = [pipe fileHandleForWriting];
+    dup2( [fileHandleForWriting fileDescriptor], fileno(stderr) );
+    
+    [NSAutoreleasePool showPools];
+    [fileHandleForWriting closeFile];
+	
+    //		fflush(stderr);
+    //		NSFileHandle *readFileHandle = [pipe fileHandleForReading];
+    //		NSData *data = [readFileHandle availableData];
+    //		
+    dup2(o,fileno(stderr));
+    close(o);
+    //		dataAsString = [[[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding] autorelease];
+    
+	return dataAsString;
+}
 
 - (void)testUnknownStuff {
 
@@ -378,48 +464,35 @@ int timeToDoWork;
 //        -- clear hardware breakpt
 //        -- really we should check dr6 that the breakpoint was matched
 
-    StandinForStepper *stepperStandin = [[StandinForStepper alloc] init];
-    
+    StandinForStepper *stepperStandin = [[StandinForStepper alloc] init];    
     RepeatingAsyncTaskController *repeatingAsyncTaskController = [[RepeatingAsyncTaskController alloc] init];
-    
-    // Ensure that these are called on the desired thread
-    CallbackProxy *stepMsg = [[[CallbackProxy alloc] init] autorelease];
-    [stepMsg setup:stepperStandin :[NSThread currentThread] :@selector(fakeStep)];
-    
-    CallbackProxy *noMoreCallback = [[[CallbackProxy alloc] init] autorelease];
-    [noMoreCallback setup:repeatingAsyncTaskController :[NSThread currentThread] :@selector(taskDidComplete)];
-    
-    CallbackProxy *errorCallback = [[[CallbackProxy alloc] init] autorelease];
-    [errorCallback setup:repeatingAsyncTaskController :[NSThread currentThread] :@selector(taskDidError)];
-    
-    CallbackProxy *stoppedCallback = [[[CallbackProxy alloc] init] autorelease];
-    [stoppedCallback setup:repeatingAsyncTaskController :[NSThread currentThread] :@selector(taskIsWaiting)];
-    
     AsyncAction *asyncTask = [[[AsyncAction alloc] init] autorelease];
-    NSDictionary *callbacks = [NSDictionary dictionaryWithObjectsAndKeys:stepMsg, @"step", 
-                                                                        noMoreCallback, @"noMoreCallback",
-                                                                        errorCallback, @"errorCallback", 
-                                                                        stoppedCallback, @"stoppedCallback", 
-                                                                        nil];
+    
+    void (^block1)() = ^ { [stepperStandin fakeStep:asyncTask];};
+    void (^block2)() = ^ { [repeatingAsyncTaskController taskDidComplete]; };
+    void (^block3)() = ^ { [repeatingAsyncTaskController taskDidError]; };
+    void (^block4)() = ^ { [repeatingAsyncTaskController taskIsWaiting]; };
+
+    NSDictionary *callbacks = [NSDictionary dictionaryWithObjectsAndKeys:   [[block1 copy] autorelease], @"step", 
+                                                                            [[block2 copy] autorelease], @"noMoreCallback",
+                                                                            [[block3 copy] autorelease], @"errorCallback", 
+                                                                            [[block4 copy] autorelease], @"stoppedCallback",                                                                         nil];
     [asyncTask setCallbacks:callbacks];
     
     [repeatingAsyncTaskController setTask:asyncTask];
     [repeatingAsyncTaskController go];
     
-    [repeatingAsyncTaskController waitUntillComplete];
+    [repeatingAsyncTaskController waitUntilAllOperationsAreFinished];
     
     STAssertTrue( stepperStandin->count == 3, nil );
     
 
-    
-
-//        }
-        
-//    }
  
-//    [instrStepper release];
-//    [codeDataBlock1 release];
-//    [dissasembler release];
+    [stepperStandin release];
+    [repeatingAsyncTaskController release];
+    
+    [NSAutoreleasePool showPools];
+
 }
 
 
