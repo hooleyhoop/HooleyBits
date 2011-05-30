4daba0b04d477e727267e5671be1c223fe7f7a73
HooleyHoop <steven.hooley@gmail.com>
2011-05-30 23:09:49 +0100

    github or dropbox?

---
+++ MachoLoader/Classes/RepeatingAsyncTaskController.h
@@ -0,0 +1,17 @@
+//
+//  RepeatingAsyncTaskController.h
+//  MachoLoader
+//
+//  Created by Steven Hooley on 29/05/2011.
+//  Copyright 2011 uk.co.stevehooley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface RepeatingAsyncTaskController : NSObject {
+@private
+    
+}
+
+@end
---
+++ MachoLoader/Tests/AsyncActionTests.m
@@ -0,0 +1,28 @@
+//
+//  AsyncActionTests.m
+//  MachoLoader
+//
+//  Created by Steven Hooley on 29/05/2011.
+//  Copyright 2011 uk.co.stevehooley. All rights reserved.
+//
+
+#import "AsyncActionTests.h"
+
+
+@implementation AsyncActionTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+@end
