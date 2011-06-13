ebbf9c2c1d4edd2928b94bdb316f9ea3582369d7
HooleyHoop <steven.hooley@gmail.com>
2011-06-13 23:41:52 +0100

    moving to xcode schemes

---
+++ HooStateMachine/Framework/HooSM.h
@@ -0,0 +1,10 @@
+//
+//  AbstractConfiguration.h
+//  HooStateMachine
+//
+//  Created by Steven Hooley on 22/05/2011.
+//  Copyright 2011 Tinsal Parks. All rights reserved.
+//
+#define HOOSM_IS_LIBRARY 1
+
+#import <HooStateMachine/HooSM_headers.h>
---
+++ HooStateMachine/HooStateMachine/HooSM_headers.h
@@ -0,0 +1,33 @@
+//
+//  AbstractConfiguration.h
+//  HooStateMachine
+//
+//  Created by Steven Hooley on 22/05/2011.
+//  Copyright 2011 Tinsal Parks. All rights reserved.
+//
+
+#if HOOSM_IS_LIBRARY
+
+#import <HooStateMachine/HooStateMachine_abstractEvent.h>
+#import <HooStateMachine/HooStateMachine_command.h>
+#import <HooStateMachine/HooStateMachine_event.h>
+#import <HooStateMachine/HooStateMachine_state.h>
+#import <HooStateMachine/HooStateMachine_transition.h>
+#import <HooStateMachine/HooStateMachine.h>
+#import <HooStateMachine/HooStateMachine_controller.h>
+#import <HooStateMachine/HooStateMachineConfigurator.h>
+#import <HooStateMachine/AbstractConfiguration.h>
+
+#else 
+
+#import "HooStateMachine_abstractEvent.h"
+#import "HooStateMachine_command.h"
+#import "HooStateMachine_event.h"
+#import "HooStateMachine_state.h"
+#import "HooStateMachine_transition.h"
+#import "HooStateMachine.h"
+#import "HooStateMachine_controller.h"
+#import "HooStateMachineConfigurator.h"
+#import "AbstractConfiguration.h"
+
+#endif
