a951054e045ef8b7b5c00d0d71a96e09093b52e5
HooleyHoop <steven.hooley@gmail.com>
2012-02-28 16:05:24 +0000

    removed state machine

--- HooStateMachine/HooStateMachine/HooSM.h
+++
@@ -1,9 +0,0 @@
-//
-//  AbstractConfiguration.h
-//  HooStateMachine
-//
-//  Created by Steven Hooley on 22/05/2011.
-//  Copyright 2011 AudioBoo. All rights reserved.
-//
-
-#import "HooSM_headers.h"
